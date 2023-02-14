37fe31114a777fe91941fd53c8f4c0562d077fe1
Alexg-z <alex.lags@outlook.es>
2023-02-14 14:43:39 -0700

    Conrreccion de errores en el tablero hecho.

--- script.js
+++ script.js
@@ -1,84 +1,80 @@
-/* Funcionalidad juego */
 let iconos = []
-
-//Función cargar tarjetas
+let selecciones = []
+generarTablero()
 function cargarIconos() {
     iconos = [
-        '<i class="fa-solid fa-hand-fist"></i>',
-        '<i class="fa-solid fa-book-skull"></i>',
-        '<i class="fa-solid fa-ghost"></i>',
-        '<i class="fa-solid fa-dragon"></i>',
-        '<i class="fa-solid fa-chess-knight"></i>',
-        '<i class="fa-solid fa-wand-sparkles"></i>',
-        '<i class="fa-solid fa-chess-rook"></i>',
-        '<i class="fa-solid fa-ring"></i>',
-        '<i class="fa-solid fa-scroll"></i>'
+        '<i class="fa-sharp fa-solid fa-star"></i>',
+        '<i class="fa-sharp fa-solid fa-gamepad"></i>',
+        '<i class="fa-sharp fa-solid fa-landmark"></i>',
+        '<i class="fa-sharp fa-solid fa-music"></i>',
+        '<i class="fa-sharp fa-solid fa-bomb"></i>',
+        '<i class="fa-sharp fa-solid fa-feather"></i>',
+        '<i class="fa-sharp fa-solid fa-check"></i>',
+        '<i class="fa-sharp fa-solid fa-dumpster-fire"></i>',
+        '<i class="fa-sharp fa-solid fa-briefcase"></i>',
+        '<i class="fa-sharp fa-solid fa-cart-shopping"></i>',
+        '<i class="fa-sharp fa-solid fa-mug-hot"></i>',
+        '<i class="fa-sharp fa-solid fa-car"></i>',
     ]
 }
-
-//Generar tablero
+//generar tablero
 function generarTablero() {
-    //Llamar funcion cargar iconos
-    cargarIconos();
+    //llamar funcion
+    cargarIconos()
+    selecciones = []
+    //representativo para html (id)
 
-    //Representativo para HTML por medio de ID
-    let tablero = document.getElementById("tablero");
-    //Arreglos for
+    let tablero = document.getElementById("tablero")
     let tarjetas = []
-
-    for (let i = 0; i < 18; i++) {
+    for (let i = 0; i < 24; i++) {
         tarjetas.push(
-            `<div class="area-tarjeta" onclick="seleccionarTarjeta(${i})>
-                <div class="tarjeta" id="tarjeta ${i}">
-                    <div class="cara trasera" id="trasera ${i}">
-                        ${iconos[0]}
-                    </div>
+            `<div class="area-tarjeta"  onclick="seleccionarTarjeta(${i})">
+                    <div class="tarjeta" id="tarjeta${i}">
+                        <div class="cara trasera" id="trasera${i}">
+                            ${iconos[0]}
+                        </div>
                     <div class="cara superior">
                         <i class="fa-solid fa-circle-question"></i>
                     </div>
                 </div>
             </div>`
         )
-
-        if (i%2==1)
-        iconos.splice(0,1)
-    }
-
-    tarjetas.sort(() => Math.random() - 0.5);
-    tablero.innerHTML = tarjetas.join(" ")
-
-}
-
-let selecciones = []
-
-function seleccionarTarjeta (i) {
-    let tarjeta = doc.getElementById("tarjeta" + i)
-    if (tarjeta.style.transform != "rotateY(180deg)"){
-        tarjeta.style.transform != "rotateY(0deg)"
-        selecciones.push(i)
-    }
-
-    if (selecciones.length == 2){
-        deseleccionar(selecciones)
-        selecciones=[]
-    }
-}
-
-function deseleccionar (selecciones) {
-    setTimeout (()=>{
-        let trasera1 = document.getElementById("trasera" + selecciones[0])
-        let trasera2 = document.getElementById("trasera" + selecciones[1])
-        
-        if(tr1.innerHTML != tr2.innerHTML){
-            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
-            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])   
-           
-            tarjeta1.style.transform="rotateY(0deg)"
-            tarjeta2.style.transform="rotateY(0deg)"
-        } else {
-            trasera1.style.background="plum"
-            trasera2.style.background="plum"
+        if (i % 2 == 1) {
+            iconos.splice(0, 1)
         }
 
-    },1000);
+    }
+    tarjetas.sort(() => Math.random() - 0.5)
+    tablero.innerHTML = tarjetas.join(" ")
+}
+function seleccionarTarjeta(i) {
+    let tarjeta = document.getElementById("tarjeta" + i)
+    if (tarjeta.style.transform != "rotateY(180deg)") {
+        tarjeta.style.transform = "rotateY(180deg)"
+        selecciones.push(i)
+    }
+    if (selecciones.length == 2) {
+        deseleccionar(selecciones)
+        selecciones = []
+    }
+
+
+}
+setInterval(tiempoJuego, 1000)
+
+function deseleccionar(selecciones) {
+    setTimeout(() => {
+        let trasera1 = document.getElementById("trasera" + selecciones[0])
+        let trasera2 = document.getElementById("trasera" + selecciones[1])
+        if (trasera1.innerHTML != trasera2.innerHTML) {
+            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
+            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
+            tarjeta1.style.transform = "rotateY(0deg)"
+            tarjeta2.style.transform = "rotateY(0deg)"
+
+        } else {
+            trasera1.style.background = "rgb(82, 4, 172)"
+            trasera2.style.background = "rgb(82, 4, 172)"
+        }
+    }, 1000);
 }
